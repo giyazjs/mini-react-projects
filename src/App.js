b25fb58c9643bd26c379785705f2975a673894dc
Giyaz Atazhanov <atazhanovgiyaz.com>
2022-08-23 17:39:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
 
 function App() {
+  const [num, setNum] = React.useState(0);
+  const [text, setText] = React.useState();
+
+  const plus = () => {
+    setNum(num + 1);
+  };
+  const minus = () => {
+    setNum(num - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="text">Число</span>
+      <h1 className="num">{num}</h1>
+      <div className="btns">
+        <button className="minusBtn" onClick={minus}>
+          Минус -
+        </button>
+        <button className="plusBtn" onClick={plus}>
+          Плюс +
+        </button>
+      </div>
     </div>
   );
 }
